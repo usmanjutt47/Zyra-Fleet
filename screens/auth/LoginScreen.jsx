5ce22ed37ce722d7b6c24c6b1ae6b64aa683b16b
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-21 11:57:27 +0500

    Update LoginScreen.jsx

--- screens/auth/LoginScreen.jsx
+++ screens/auth/LoginScreen.jsx
@@ -6,91 +6,262 @@ import {
   View,
   Image,
   TextInput,
+  ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const { width, height } = Dimensions.get("window");
 
-const rf = (size) => (size * width) / 375;
-const rp = (size) => (size * width) / 375;
-const rw = (size) => (size * height) / 812;
-const rh = (size) => (size * width) / 375;
+const responsiveFontSize = (size) => (size * width) / 375;
+const responsivePadding = (size) => (size * width) / 375;
+const responsiveWidth = (size) => (size * height) / 812;
+const responsiveHeight = (size) => (size * width) / 375;
 
 export default function LoginScreen() {
+  const [isTicked, setIsTicked] = useState(false);
   const navigation = useNavigation();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <View style={{ width: "95%", height: "100%", alignSelf: "center" }}>
-        <Pressable
-          style={styles.backButtonContainer}
-          onPress={() => navigation.goBack()}
-        >
-          <Ionicons name="chevron-back" color="#444444" size={25} />
-        </Pressable>
-        <Text style={styles.heading}>Login Account</Text>
-        <Text style={styles.desc}>
-          Fill the information and for create a account?
-        </Text>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={{ width: "90%", height: "100%", alignSelf: "center" }}>
+          <Pressable
+            style={styles.backButtonContainer}
+            onPress={() => navigation.goBack()}
+          >
+            <Ionicons name="chevron-back" color="#444444" size={25} />
+          </Pressable>
+          <Text style={styles.heading}>Login Account</Text>
+          <Text style={styles.desc}>
+            Fill the information and for create a account?
+          </Text>
 
-        <View>
-          <Text style={{ marginTop: rh(40) }}>Email</Text>
-          <View style={styles.inputContainer}>
-            <Pressable style={styles.iconContainer}>
-              <Image
-                source={require("../../assets/icons/email.png")}
-                style={styles.inputIcon}
+          <View>
+            <Text style={{ marginTop: responsiveHeight(40) }}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Pressable style={styles.iconContainer}>
+                <Image
+                  source={require("../../assets/icons/email.png")}
+                  style={styles.inputIcon}
+                />
+              </Pressable>
+              <TextInput
+                placeholder="Enter your email address"
+                style={styles.input}
+                keyboardType="email-address"
+                placeholderTextColor={"#7C7C7C"}
               />
+            </View>
+          </View>
+
+          <View>
+            <Text style={{ marginTop: responsiveHeight(20) }}>Password</Text>
+            <View style={styles.inputContainer}>
+              <Pressable style={styles.iconContainer}>
+                <Image
+                  source={require("../../assets/icons/password.png")}
+                  style={styles.inputIcon}
+                />
+              </Pressable>
+              <TextInput
+                placeholder="Enter your Password"
+                style={styles.input}
+                placeholderTextColor={"#7C7C7C"}
+                secureTextEntry={true}
+              />
+            </View>
+          </View>
+          <View
+            style={[
+              styles.forgetPasswordContainer,
+
+              styles.forgetPasswordContainer2,
+            ]}
+          >
+            <Pressable
+              style={{ flexDirection: "row" }}
+              onPress={() => setIsTicked(!isTicked)}
+            >
+              <Pressable style={styles.container}>
+                {isTicked ? <Text style={styles.tickIcon}>✔</Text> : null}
+              </Pressable>
+              <Text style={styles.savePasswword}>Save password</Text>
             </Pressable>
-            <TextInput
-              placeholder="Enter your email address"
-              style={styles.input}
-              keyboardType="email-address"
-              placeholderTextColor={"#7C7C7C"}
-            />
+
+            <Pressable onPress={() => navigation.navigate("ForgotPassword")}>
+              <Text style={styles.forgetText}>Forgot Password?</Text>
+            </Pressable>
+          </View>
+          <Pressable
+            style={styles.buttonContainer}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text style={styles.buttonText}>Sign in</Text>
+          </Pressable>
+          <Pressable style={styles.textOuterContainer}>
+            <View style={styles.textMainContainer}>
+              <Pressable>
+                <Text>Do not have an account? </Text>
+              </Pressable>
+              <Pressable
+                style={styles.textContainer}
+                onPress={() => navigation.navigate("SignUp")}
+              >
+                <Text style={styles.signUpText}>Sign up</Text>
+              </Pressable>
+            </View>
+          </Pressable>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: responsiveHeight(40),
+            }}
+          >
+            <View style={{ flex: 1, height: 1, backgroundColor: "#C7C7C7" }} />
+            <Text style={{ marginHorizontal: 10 }}>or</Text>
+            <View style={{ flex: 1, height: 1, backgroundColor: "#C7C7C7" }} />
+          </View>
+          <View style={{ marginTop: responsiveHeight(40) }}>
+            <Pressable style={styles.facebook}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+              >
+                <Pressable
+                  style={{
+                    height: responsiveHeight(30),
+                    width: responsiveWidth(30),
+                    borderRadius: 99,
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <EvilIcons name="sc-facebook" size={30} color="#4267B2" />
+                </Pressable>
+                <Text
+                  style={{
+                    marginLeft: responsiveHeight(5),
+                    fontWeight: "regular",
+                  }}
+                >
+                  Continue with Facebook
+                </Text>
+              </View>
+            </Pressable>
+            <Pressable
+              style={[styles.facebook, { marginTop: responsiveHeight(10) }]}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+              >
+                <Pressable
+                  style={{
+                    height: responsiveHeight(30),
+                    width: responsiveWidth(30),
+                    borderRadius: 99,
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: -20,
+                  }}
+                >
+                  <Image
+                    source={require("../../assets/icons/google.png")}
+                    style={{ height: 17, width: 17 }}
+                  />
+                </Pressable>
+                <Text
+                  style={{
+                    marginLeft: responsiveHeight(5),
+                    fontWeight: "regular",
+                    fontWeight: "regular",
+                  }}
+                >
+                  Continue with Google
+                </Text>
+              </View>
+            </Pressable>
+            <Pressable
+              style={[
+                styles.facebook,
+                {
+                  marginTop: responsiveHeight(10),
+                  marginBottom: responsiveHeight(40),
+                },
+              ]}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+              >
+                <Pressable
+                  style={{
+                    height: responsiveHeight(30),
+                    width: responsiveWidth(30),
+                    borderRadius: 99,
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: -30,
+                  }}
+                >
+                  <AntDesign name="apple1" size={17} color="black" />
+                </Pressable>
+                <Text
+                  style={{
+                    marginLeft: responsiveHeight(5),
+                    fontWeight: "regular",
+                  }}
+                >
+                  Continue with Apple
+                </Text>
+              </View>
+            </Pressable>
           </View>
         </View>
-
-        <View>
-          <Text style={{ marginTop: rh(20) }}>Password</Text>
-          <View style={styles.inputContainer}>
-            <Pressable style={styles.iconContainer}>
-              <Image
-                source={require("../../assets/icons/password.png")}
-                style={styles.inputIcon}
-              />
-            </Pressable>
-            <TextInput
-              placeholder="Enter your Password"
-              style={styles.input}
-              placeholderTextColor={"#7C7C7C"}
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   backButtonContainer: {
-    width: rw(37),
-    height: rh(35),
+    width: responsiveWidth(37),
+    height: responsiveHeight(35),
     backgroundColor: "#f1f1f1",
     borderRadius: 6,
     justifyContent: "center",
     alignItems: "center",
   },
   heading: {
-    fontSize: rf(28),
+    fontSize: responsiveFontSize(28),
     fontFamily: "semiBold",
-    marginTop: rh(20),
+    marginTop: responsiveHeight(20),
   },
   desc: {
-    fontSize: rf(12),
+    fontSize: responsiveFontSize(12),
     fontFamily: "regular",
     color: "#9D9D9D",
   },
@@ -99,11 +270,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F9F9F9",
     borderRadius: 8,
-    height: rh(48),
-    marginTop: rh(5),
+    height: responsiveHeight(48),
+    marginTop: responsiveHeight(5),
   },
   input: {
-    fontSize: rf(12),
+    fontSize: responsiveFontSize(12),
     flex: 1,
     borderRadius: 8,
     color: "#000",
@@ -114,12 +285,92 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   iconContainer: {
-    height: rh(27),
-    width: rw(27),
+    height: responsiveHeight(27),
+    width: responsiveWidth(27),
     borderRadius: 30,
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
     marginHorizontal: 10,
+  },
+  forgetPasswordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: responsiveHeight(20),
+  },
+  forgetText: {
+    textAlign: "right",
+    color: "#92499C",
+    fontFamily: "regular",
+  },
+  savePasswword: {
+    fontSize: responsiveFontSize(12),
+    color: "#7C7C7C",
+    fontFamily: "regular",
+  },
+  container: {
+    height: responsiveHeight(15),
+    width: responsiveWidth(15),
+    borderWidth: 1,
+    borderColor: "#D2D2D2",
+    marginRight: responsiveWidth(5),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+  },
+  forgetPasswordContainer2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  buttonContainer: {
+    height: responsiveHeight(48),
+    width: "100%",
+    backgroundColor: "#92499C",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    marginTop: responsiveHeight(40),
+  },
+  buttonText: {
+    fontSize: 14,
+    fontFamily: "regular",
+    color: "#ffff",
+  },
+  signUpText: {
+    color: "#92499C",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  textContainer: {
+    alignItems: "center",
+    alignContent: "center",
+    alignSelf: "center",
+  },
+  textMainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  textOuterContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: responsivePadding(10),
+  },
+  facebook: {
+    height: responsiveHeight(48),
+    width: "100%",
+    backgroundColor: "#F9F9F9",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  tickIcon: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    lineHeight: responsiveHeight(12),
+    color: "#D2D2D2",
+    fontSize: 10,
   },
 });
