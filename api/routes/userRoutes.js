3fc47bc94f6500c3e596f45923964b4b92de7b88
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-25 11:59:39 +0500

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/userController");
+const { createEntry, getEntries } = require("../controllers/TruckEntryController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.post("/truck-entry", createEntry);
+router.get("/truck-entry/:userId", getEntries);
 
 module.exports = router;
